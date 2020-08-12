631973ca2c18882a587d377522e942ccb4605d55
Ahmad Faizal B H <ahmadfaizalbh726@gmail.com>
2020-08-12 18:04:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='quick_secure',
-    version='0.2.0',
+    version='0.2.1',
     py_modules=['quick_secure'],
     author="Ahmad Faizal B H",
     author_email="ahmadfaizalbh726@gmail.com",
