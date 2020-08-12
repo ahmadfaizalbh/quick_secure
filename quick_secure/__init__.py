388448e67d3748b6f9942d9d6a67d6edf622309e
Ahmad Faizal B H <ab4953@columbia.edu>
2020-08-12 06:39:18 +0530

    fix surrogates not allowed

--- quick_secure/__init__.py
+++ quick_secure/__init__.py
@@ -1,20 +1,45 @@
 import base64
+from itertools import cycle
 
-def shuffle(code):
-  return "".join(i+j  for i,j in zip(code[::2],code[1::2][::-1]+"=")).strip("=")
 
-def encode(string,key):
-    l=len(key)
-    ens = "".join(chr(ord(s) + ord(key[i % l])+l-i) for i,s in enumerate(string))
+def _shuffle(code):
+    return "".join(i + j for i, j in zip(code[::2], code[1::2][::-1] + "=")).strip("=")
+
+
+def _build_char_right(num):
+    num %= 1112064
+    if num > 55295:
+        num += 2048
+    return chr(num)
+
+
+def _build_bin_left(char, pos):
+    num = ord(char)
+    if num > 55295:
+        if num < 57344:
+            raise UnicodeError(
+                f"'utf-8' codec can't encode character '{char}' "
+                f"in position {pos}: surrogates not allowed")
+        num -= 2048
+    return num
+
+
+def encode(string, key):
+    length = len(string)
+    key_codes = cycle(map(ord, key))
+    ens = "".join(_build_char_right((_build_bin_left(s, i) + next(key_codes) + length - i))
+                  for i, s in enumerate(string))
     code = base64.urlsafe_b64encode(ens.encode()).decode().strip("=")
-    return shuffle(code)
-
-def decode(decode_string,key):
-    string = shuffle(decode_string)
-    padding = 4-(len(string)%4)
-    if padding!=4:string += "="*padding
-    l=len(key)
-    return "".join(chr(ord(s) + i - (ord(key[i % l]) + l))
-                   for i,s in enumerate(base64.urlsafe_b64decode(string).decode()))
+    return _shuffle(code)
 
 
+def decode(decode_string, key):
+    string = _shuffle(decode_string)
+    padding = 4 - (len(string) % 4)
+    if padding != 4:
+        string += "=" * padding
+    key_codes = cycle(map(ord, key))
+    string = base64.urlsafe_b64decode(string).decode()
+    length = len(string)
+    return "".join(_build_char_right((_build_bin_left(s, i) + i - next(key_codes) - length))
+                   for i, s in enumerate(string))
